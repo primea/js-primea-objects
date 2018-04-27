bbffc1cd4f29e05f96fa2d1d15e71595b17bfc7f
Norton Wang <nortantivirus@gmail.com>
2018-04-27 12:33:23 -0400

    use class methods for json stuff

--- tests/index.js
+++ tests/index.js
@@ -79,7 +79,10 @@ tape('utils', t => {
     Buffer.from([1, 2, 3, 4])
   ]
   const json = utils.toJSON(obj)
-  t.deepEquals(JSON.stringify(json), '[{"@ModuleRef":{"id":"0x01"}},{"@FunctionRef":{"id":"0x01","private":false,"name":"main","gas":100}},"0x01020304"]')
+  t.deepEquals(JSON.stringify(json), '[{"@ModuleRef":{"id":"0x01"}},{"@FunctionRef":{"actorID":"0x01","private":false,"name":"main","gas":100}},"0x01020304"]')
+
+  const jsonFull = utils.toJSON(obj, true)
+  t.deepEquals(JSON.stringify(jsonFull), '[{"@ModuleRef":{"id":"0x01","exports":{"name":["i32"]}}},{"@FunctionRef":{"actorID":"0x01","private":false,"name":"main","gas":100,"params":["i32"]}},"0x01020304"]')
 
   const newObj = [
     new objects.ModuleRef(undefined, id),
@@ -91,6 +94,8 @@ tape('utils', t => {
     Buffer.from([1, 2, 3, 4])
   ]
   t.deepEquals(utils.fromJSON(json), newObj)
+
+  t.deepEquals(utils.fromJSON(jsonFull), obj)
 
   const hashedId01 = utils.actorRefToId([0, 1])
   t.deepEquals(hashedId01.id, Buffer.from('0ca311b75efd27e7daf6eec8b51b5c1fe33ff233', 'hex'))
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-const { ID, FunctionRef, ModuleRef, generateActorId, getType } = require('./')
+const { FunctionRef, ModuleRef, generateActorId, getType } = require('./')
 
 const actorRefToId = ref => ref.reduce((parent, curr) => generateActorId({ parent, nonce: curr }), null)
 
@@ -6,31 +6,18 @@ const toHex = arg => Buffer.isBuffer(arg) ? `0x${arg.toString('hex')}` : arg
 
 const fromHex = arg => typeof arg !== 'string' ? arg : Buffer.from(arg.slice(0, 2) === '0x' ? arg.slice(2) : arg, 'hex')
 
-const toJSON = arg => {
+const toJSON = (arg, includeOptional = false) => {
   switch (getType(arg)) {
     case 'elem':
-      return arg.map(toJSON)
+      return arg.map(a => toJSON(a, includeOptional))
     case 'id':
-      return toJSON(arg.id)
     case 'func':
-      return {
-        '@FunctionRef': {
-          id: toJSON(arg.actorID),
-          private: arg.identifier[0],
-          name: arg.identifier[1],
-          gas: arg.gas
-        }
-      }
     case 'mod':
-      return {
-        '@ModuleRef': {
-          id: toJSON(arg.id)
-        }
-      }
+      return arg.toJSON(includeOptional)
     case 'link':
       return {
         '@Link': {
-          '/': toJSON(arg['/'])
+          '/': toJSON(arg['/'], includeOptional)
         }
       }
     case 'data':
@@ -46,21 +33,9 @@ const fromJSON = arg => {
     }
 
     if (arg['@FunctionRef']) {
-      const data = arg['@FunctionRef']
-      return new FunctionRef({
-        identifier: [data.private, data.name],
-        actorID: new ID(fromJSON(data.id)),
-        params: data.params,
-        gas: data.gas
-      })
+      return FunctionRef.fromJSON(arg)
     } else if (arg['@ModuleRef']) {
-      const data = arg['@ModuleRef']
-      return new ModuleRef(data.exports, new ID(fromJSON(data.id)))
-    } else if (arg['@Link']) {
-      const data = arg['@Link']
-      return {
-        '/': fromJSON(data['/'])
-      }
+      return ModuleRef.fromJSON(arg)
     }
   }
 
